b3ca2f1340a0e351581de93924ce5e7f7f088a82
Reza Pramudhika <rezapramudhika@gmail.com>
2018-02-09 10:48:52 +0700

    commit-3

--- medium/lubang-terbesar.js
+++ medium/lubang-terbesar.js
@@ -10,14 +10,37 @@ function lubangTerbesar(arr){
 
     let trap = 0;
 
-    // for(let i=0; i<board.length; i++){
-    //     for(let j=0; j)
-    // }
+    for(let i=0; i<board.length; i++){
+        for(let j=0; j<board[i].length; j++){
+            if(board[i][j]==0){
+                let trapHorizontal = 0;
+                for(let k=1; k<board[i].length; k++){
+                    if(board[i][k]==0){
+                        trapHorizontal += 1;
+                    }
+                }
+                if(trap<trapHorizontal){
+                    trap = trapHorizontal;
+                }
 
+                // let trapVertical = 0;
+                // for(let k=1; k<board[i].length; k++){
+                //     if(board[j][k]==0){
+                //         trapVertical += 1;
+                //     }
+                // }
+                // if(trap<trapVertical){
+                //     trap = trapVertical;
+                // }
+            }
+        }
+    }
 
-    return board;
+    console.log(board);
+
+    return trap;
 }
 
 console.log(lubangTerbesar(["00111", "01101", "00100", "11110"])); // 3
-// console.log(lubangTerbesar(["111", "111", "111", "100"])); // 2
-// console.log(lubangTerbesar(["00111", "10011", "00111", "10010","00110",'10111'])); // 6
+console.log(lubangTerbesar(["111", "111", "111", "100"])); // 2
+console.log(lubangTerbesar(["00111", "10011", "00111", "10010","00110",'10111'])); // 6
