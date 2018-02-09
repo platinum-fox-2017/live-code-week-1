48d44f4e7d954f103c9cfdfe0200c273dd761bfe
Ervan Adetya <vaanz.adetya@gmail.com>
2018-02-09 10:48:38 +0700

    Second Commit

--- challenging/kurung.js
+++ challenging/kurung.js
@@ -2,14 +2,30 @@
 
 function generateParen(num) {
     let arrPosibilities=[]
-    let tempPosibilities='';
-    let paren=['(',')']
+    let tempPosibilities=[];
+    let paren=['(',')'];
+    let mid=0;
     for(let i=0; i<num; i++) {
-        let mid=(tempPosibilities.length-1)/2
-        tempPosibilities.splice(mid,0,paren)
+        tempPosibilities.push(paren[0],paren[1]);
+        console.log(tempPosibilities.join(''));
+        let tempPosibilities2=tempPosibilities.slice();
+        for(let j=0; j<num-i-1; j++) {
+            tempPosibilities2.push(paren[1])
+            tempPosibilities2.splice(0,0,paren[0])
+        }
+        if (arrPosibilities.indexOf(tempPosibilities2.join(''))===-1) {
+            arrPosibilities.push(tempPosibilities2.join(''));
+        }
+        tempPosibilities2=tempPosibilities.slice();
+        for(let j=0; j<num-i-1; j++) {
+            tempPosibilities2.splice((i*2),0,paren[0]);
+            tempPosibilities2.push(paren[1]);
+        }
+        arrPosibilities.push(tempPosibilities2.join(''));
+        tempPosibilities2=tempPosibilities.slice();
     }
-    arrPosibilities.push(tempPosibilities);
     return arrPosibilities;
 }
 
 console.log(generateParen(4));
+console.log(generateParen(3));
