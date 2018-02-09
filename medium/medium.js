c4e2777600ff600d7d041c61cb9d9992b8c25368
Ozi <ozi@Ozis-MacBook-Pro.local>
2018-02-09 10:48:39 +0700

    Update 1.5hr

--- medium/medium.js
+++ medium/medium.js
@@ -1,28 +1,32 @@
 let lubangTerbesar = (input) => {
-    console.log(input);
-let max = 0;
 
-    for(let i=0; i<input[0].length; i++){
-        console.log(input[0])
-        console.log(input[1])
-        console.log(input[2])
-        console.log(input[3])
-        for(let j=0; i<input[1].length; j++){
-            for(let k=0; k<input[2].length; k++){
-                for(let l=0; l<input[3].length; l++){
-                    if(i === j && i === k && i === l){
-                        if(input[i] === 0 
-                            && input[j] === 0 
-                            && input[k] === 0 
-                            && input[l] === 0){
-                                max += 4;
-                            }
-                    }
-                }
+    let count_horizontal1 = 0
+    let count_horizontal2 = 0
+    let count_horizontal3 = 0
+    let count_horizontal4 = 0
+    let count_vertical1 = 0
+    let count_vertical2 = 0
+    let count_vertical3 = 0
+    let count_vertical4 = 0
+
+    console.log(input[0]);
+    console.log(input[1]);
+    console.log(input[2]);
+    console.log(input[3]);
+    let zero_positions = [];
+    for(let i=0; i<input.length; i++){
+        for(let j=0; j<input[i].length; j++){
+            if(parseInt(input[i][j]) === 0){
+                zero_positions.push([i,j]);
             }
         }
     }
+    // console.log(zero_positions);
+    for(let i=0; i<zero_positions.length; i++){
+        console.log(zero_positions[i][0]);
+      
+    }
 }
 console.log(lubangTerbesar(["00111", "01101", "00100", "11110"])); // 3
-console.log(lubangTerbesar(["111", "111", "111", "100"])); // 2
-console.log(lubangTerbesar(["00111", "10011", "00111", "10010","00110",'10111'])); // 6
+// console.log(lubangTerbesar(["111", "111", "111", "100"])); // 2
+// console.log(lubangTerbesar(["00111", "10011", "00111", "10010","00110",'10111'])); // 6
