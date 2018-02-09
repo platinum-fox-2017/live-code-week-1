53ef07416ecb90fa21dc36cf50cd51f57e5894d3
agrha <agrhakartz@gmail.com>
2018-02-09 11:39:37 +0700

    commit 3

--- easy/hourglass.js
+++ easy/hourglass.js
@@ -1,28 +1,30 @@
 function hourGlass(index) {
   let arr = []
-  for (let i = 0 ;i<index;i++){
+  for (let i = 0;i<Math.floor(index/2);i++){
     let temp = []
-    for (let j = 0;j<index;j++){
-        if(j>Math.floor(i/2)){
-          temp.push('*')
-        } else {
-          temp.push(' ')
-        }
+    for (let j = 0;j<index-i;j++){;
+      if(j>=i){
+        temp.push('*')
+      } else {
+        temp.push(' ')
+      }
     }
     arr.push(temp)
   }
+  for (let i = Math.floor(index/2);i>=0;i--){
+    let temp = []
+    for (let j = 0;j<index-i;j++){;
+      if(j>=i){
+        temp.push('*')
+      } else {
+        temp.push(' ')
+      }
+    }
+    arr.push(temp)
+  }
+
+
   return arr
 }
 
-console.log(hourGlass(5));
-
-function print(n) {
-    for(i=n;i>=1;i--) {
-        for(j=1;j<=i;j++) {
-            console.log(i+" ");
-        }
-         console.log("<br>");
-    }
-}
-
-console.log(print(5));
+console.log(hourGlass(7));
--- medium/jalan_bolong.js
+++ medium/jalan_bolong.js
@@ -5,14 +5,32 @@ function lubangTerbesar(array){
     arr.push(array[i].split(''))
   }
 
-  // check Vertical
-  for (var i = 0; i < array.length; i++) {
-    let count = []
-    for (var j = 0; j < array.length; j++) {
-      if
+  // check horizontal
+  for (var i = 0; i < arr.length; i++) {
+    let count = 0
+    for (var j = 0; j < arr[i].length; j++) {
+      if(arr[i][j] === '0' && arr[i][j+1] === '0'){
+        count++
+      } else {
+        arrCount.push(count)
+      }
     }
   }
 
+
+  // check vertical
+  for (var i = 0; i < arr.length-1; i++) {
+    let count = 0
+    for (var j = 0; j < arr[i].length; j++) {
+      if(arr[i][j] === '0' && arr[i+1][j] === '0'){
+        count++
+      } else {
+        arrCount.push(count)
+      }
+    }
+  }
+  console.log(arr);
+  return arrCount.sort()[arrCount.length-1]+1
 }
 
 console.log(lubangTerbesar(["00111", "01101", "00100", "11110"])); // 3
