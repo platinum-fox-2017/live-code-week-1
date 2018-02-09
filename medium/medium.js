4dfd1156a862f31b935709e7b95a6cd09846b3c3
agungcaproex <agung.caproex@gmail.com>
2018-02-09 11:47:26 +0700

    commit the end...

--- medium/medium.js
+++ medium/medium.js
@@ -1,15 +1,25 @@
 function lubangTerbesar(input){
   var arr = []
+  var count = 0
   var string = input.toString()
+  var save = []
 
   for(let i=0; i<input.length; i++){
     arr.push([])
-
     for(let j=0; j<input[i].length; j++){
-      arr[i].push(string[j])
+
+      arr[i].push(string[j][i])
     }
   }
   return arr
+  if(count < input.length){
+    for(let k=0; k<arr[i].length; k++){
+      arr[j] = string[i][j]
+      if(string[i][j] === arr[j])
+    }
+  }
+  count++
+
 }
 
 console.log(lubangTerbesar(["00111", "01101", "00100", "11110"])); // 3
