44e2dc210c15a14b97d9abc58cf32ab9f424cc9e
Herby Herado <herby,herado@gmail.com>
2018-02-09 11:21:34 +0700

    commit: 4th 30 mins

--- challenging/challenging.js
+++ challenging/challenging.js
@@ -1,15 +1,35 @@
 function generateParen(num){
-    let result = [];
+    let tempResult = []
+    let str = ''
+    let leftParen = '('
+    let rightParen = ')'
+    let leftParenCount = 0
+    let rightParenCount = 0
     let braces = '()'
-    
-    for(i = 0; i < num; i++){
-        result.push(braces)
-    }
-    result.push(braces.repeat(num-1))    
-    result.push(braces.repeat(num))
-    console.log(result)
 
-    // console.log(result)
+    for (let i = 0; i < num; i++){
+        tempResult.push(braces.repeat(i+1))
+    }
+    
+    // console.log(tempResult)
+    let processing = []
+
+    for (let i = 0; i <tempResult.length-1; i++){
+       processing.push(tempResult[i])
+       for (let j = 0; j < processing[i].length; j++){
+            if(processing[i][j] == leftParen){
+                leftParenCount++
+            } else if (processing[i][j] == rightParen){
+                rightParenCount++
+            }
+       }
+       switch(leftParenCount){
+           case 1: tempResult.unshift('('.repeat(num-leftParenCount) + processing[i] + ')'.repeat(num-rightParenCount)); break;
+           case 2: tempResult.unshift('('.repeat(num-leftParenCount) + processing[i] + ')'.repeat(num-rightParenCount)); break;
+       }
+    }
+    console.log(tempResult)
+    // console.log(process)
 }
 
 generateParen(3) // [ '((()))', '(()())', '(())()', '()(())', '()()()' ]
@@ -30,27 +50,3 @@ generateParen(4) //
   '()()(())',
   '()()()()' ]
   */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
