0648a97c642b7475bb25f7956ed816736e475d0b
Ardhiansyah Putra <ardhimetanaputra@gmail.com>
2018-02-09 10:32:49 +0700

    Release: Lubang Terbesar.js

--- medium/Lubang Terbesar.js
+++ medium/Lubang Terbesar.js
@@ -1,19 +1,40 @@
 function lubangTerbesar(arr) {
 	let max = 0;
-	// return arr[0].match(/[0]/g);
-	for (var i = 0; i < arr.length; i++)
-		if (arr[i].match(/[0]/g) && max < arr[i].match(/[0]/g).length) max = arr[i].match(/[0]/g).length;
+	for (var i = 0; i < arr.length; i++) {
+		for (var j = 0; j < arr[i].length; j++) {
+			if (isLubang(arr[i][j])) {
+				if (max < hitungLubang(arr[i], j)) {
+					max = hitungLubang(arr[i], j);
+				}
+			}
+		}
+	}
 
 	for (let i = 0; i < arr[0].length; i++) {
 		let temp = '';
 		for (let j = 0; j < arr.length; j++) {
 			temp += arr[j][i];
 		}
-		// console.log(temp);
-		if (temp.match(/[0]/g) && max < temp.match(/[0]/g).length) max = temp.match(/[0]/g).length;
-	}
 
+		for (let j = 0; j < temp.length; j++) {
+			if (isLubang(temp[j])) {
+				if (max < hitungLubang(temp, j)) {
+					max = hitungLubang(temp, j);
+				}
+			}
+		}
+	}
 	return max;
+}
+
+function hitungLubang(str, index) {
+	if (!isLubang(str[index])) return 0;
+	else return 1 + hitungLubang(str, index+1);
+}
+
+function isLubang(str) {
+	if (str == 0) return true;
+	else return false;
 }
 
 console.log(lubangTerbesar(["00111", "01101", "00100", "11110"])); // 3
