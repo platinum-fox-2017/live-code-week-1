572bfc2544b892b1404f70fe9325afe1f7c369a6
haripermadi <harinurpermadi@gmail.com>
2018-02-09 11:47:04 +0700

    livecode

--- medium/lubang.js
+++ medium/lubang.js
@@ -17,22 +17,30 @@ function lubangTerbesar(input){
 			}
 		}
 	}
-	// console.log(posisi0)
+	console.log(posisi0)
 	let count0Vert = 0
 	let count0Hor = 0
-	// let test0 = []
-	for(let i =0;i<posisi0.length;i++){
-		for(let j =0;j<posisi0.length;j++){
-			// console.log('i',i,'j',j,'posisi',posisi0[i][1])
-		if(j === posisi0[i][1] && i !==j){
-			// test0.push([posisi0[i][1]])
+	let test0 = []
+	let count = 0
+	for(let i =0;i<posisi0.length-1;i++){
+		for(let j =0;j<posisi0[i].length;j++){
+			console.log('i',i,'j',j,'posisi',posisi0[i][1])
+			// console.log(posisi0[i][1],"ini 0,1")
+		if(posisi0[i][1] === posisi0[i+j][1] && ((i+1) % i) === 0){
+			//indexof
+			test0.push([posisi0[i][1]])
+			// count++
 			count0Vert+=1
 		}
+		// else if(posisi0[i][0]=== posisi0[i+1][0]){
+		// 	count0Hor+=1
+
+		// }
 		}
 		
 		// console.log('i',i,'posisi',posisi0[i][1])
 	}
-	// console.log(test0)
+	console.log(test0)
 	console.log(count0Vert,"ini ver")
 	console.log(count0Hor,"ini hor")
 	if(count0Vert>count0Hor){
