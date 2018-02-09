812d5f9af1d52eddd57aaca2202d1ddbef1f7572
Devdots <devdots@Devdotss-MacBook-Pro.local>
2018-02-09 11:15:20 +0700

    update

---
+++ challenging/challengin.js
@@ -0,0 +1,14 @@
+function generateParen(num) {
+  for (var i = 0; i < num; i++) {
+    var arr = []
+    for (var j = i; j < num; j++) {
+      if () {
+        arr.push('(')
+
+      }
+    }
+  }
+  return arr
+}
+
+console.log(generateParen(2));
