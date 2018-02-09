174a6388fc4bc06d8b7c0c4e00909c0a1a67a64a
FTeddy <ainsophoure@gmail.com>
2018-02-09 10:17:39 +0700

    easy med

--- easy/easy.js
+++ easy/easy.js
@@ -1,20 +1,20 @@
 function generateHourglass(number) {
   let arrayStr = [];
   // loop from 0 to number
-  for (let i = 0; i < number; i++) {
+  for (let i = 0; i < (2*number)-1; i++) {
     let hash =''
-    for (let j = number-i; j > 0; j--) {
+    for (let j = (2*number)-1-i; j > 0; j--) {
       hash += '#'
     }
-    console.log(hash);
+    // console.log(hash);
     arrayStr.push(hash);
   }
-  for (let i = number; i > 0; i--) {
+  for (let i = (2*number)-1; i > 0; i--) {
     let hash ='#'
-    for (let j = 0; j < number -i ; j++) {
+    for (let j = 0; j < (2*number)-1-i ; j++) {
       hash += '#'
     }
-    console.log(hash);
+    // console.log(hash);
     if (hash !=='#') {
       arrayStr.push(hash);
     }
@@ -24,17 +24,30 @@ function generateHourglass(number) {
 
 function printHourglass(arrayStr) {
   let logHash= [];
-  for (var i = 0; i < arrayStr; i++) {
-    arrayStr[i]
+  for (var i = 0; i < arrayStr.length; i++) {
+    // console.log(i);
+    let hashes = []
+
+    for (let j = 0; j < i; j++) {
+      hashes.push(' ')
+    }
+
+    hashes.push(arrayStr[i])
+
+    hashes.push(' ')
+
+    hashes.join('')
+    logHash.push(hashes)
   }
 
 
   // as hourglass
-  return
+  return logHash
 }
 
 // num > 0
 let hrglass = generateHourglass(5);
 console.log(typeof hrglass);
-// console.log(hrglass);
+console.log(hrglass);
+
 // console.log(printHourglass(hrglass));
