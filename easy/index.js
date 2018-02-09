2f21cf4c45b29386c084409fe1e79db3f3db1bb7
Andrew Kusuma <andrew.budikusuma@gmail.com>
2018-02-09 10:17:12 +0700

    30 menit 2

--- easy/index.js
+++ easy/index.js
@@ -1,17 +1,33 @@
 const generateHourglass = (number) => {
     let array = []
+    let count = number + (number - 1)
     for (let i = 0; i < number + (number - 1); i++) {
-        array.push('x')
-        // for (let j = 0; j < number; j++) {
-        //     array[i].push('#')
-        // }
-        // for (let index = 0; index < array.length; index++) {
-        //     const element = array[index];
 
+        // for (let j = 0; j < number + (number - 1); j++) {
+        //     let char = '#'
+        //     parseInt(char)
+        //     char *= j + 1
+        //     array[i] += char
         // }
-
+        let char = '#'
+        // console.log(1 * char)
+        if (i < number) {
+            array.push(char.repeat(count))
+            console.log('masuk sini')
+            console.log(array)
+            //push yg full trus kurangi sebanyak counter 2 dst
+            count -= 2
+            console.log(count)
+        } else if (i >= number) {
+            array.push(char.repeat(count))
+            count += 2
+        }
+        //     //push ke # ditambah trus
+        //     array.push(char.repeat(count))
+        //     count += 2
+        // }
     }
-    return array
+    // return array
 }
 
 
