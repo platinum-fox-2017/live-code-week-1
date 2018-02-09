e964cb4a37554521e7c3a5628363b89cbb506e68
Ervan Adetya <vaanz.adetya@gmail.com>
2018-02-09 11:47:53 +0700

    Last Commit

--- challenging/kurung.js
+++ challenging/kurung.js
@@ -7,11 +7,17 @@ function generateParen(num) {
     let mid=0;
     for(let i=0; i<num; i++) {
         tempPosibilities.push(paren[0],paren[1]);
-        console.log(tempPosibilities.join(''));
         let tempPosibilities2=tempPosibilities.slice();
         for(let j=0; j<num-i-1; j++) {
             tempPosibilities2.push(paren[1])
             tempPosibilities2.splice(0,0,paren[0])
+            let tempPosibilities3=tempPosibilities2.slice();
+            for(let k=j; k<num-i-2; k++) {
+                tempPosibilities3.push(paren[0],paren[1]);
+            }
+            if (arrPosibilities.indexOf(tempPosibilities3.join(''))===-1) {
+                arrPosibilities.push(tempPosibilities3.join(''));
+            }
         }
         if (arrPosibilities.indexOf(tempPosibilities2.join(''))===-1) {
             arrPosibilities.push(tempPosibilities2.join(''));
@@ -20,8 +26,18 @@ function generateParen(num) {
         for(let j=0; j<num-i-1; j++) {
             tempPosibilities2.splice((i*2),0,paren[0]);
             tempPosibilities2.push(paren[1]);
+            let tempPosibilities3=tempPosibilities2.slice();
+            for(let k=j; k<num-i-2; k++) {
+                tempPosibilities3.push(paren[0],paren[1]);
+            }
+            if (arrPosibilities.indexOf(tempPosibilities3.join(''))===-1) {
+                arrPosibilities.push(tempPosibilities3.join(''));
+            }
         }
-        arrPosibilities.push(tempPosibilities2.join(''));
+        // console.log(tempPosibilities2.join(''))
+        if (arrPosibilities.indexOf(tempPosibilities2.join(''))===-1) {
+            arrPosibilities.push(tempPosibilities2.join(''));
+        }
         tempPosibilities2=tempPosibilities.slice();
     }
     return arrPosibilities;
