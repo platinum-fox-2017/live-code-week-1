26c2c928f0d57a34ee9ecc57234c930481fd1d24
root <irsyadpahlapi28@gmail.com>
2018-02-08 22:10:17 -0500

    upload file easy finish

--- easy/index.js
+++ easy/index.js
@@ -1,18 +1,46 @@
 
-function printHourglass(num){
-
+function generateHourglass(num){
+  var simpan=[]
   for(let i=num;i>0;i--){
-    var kosong=' ',pagaratas='#'
-    for(let j=(num*2)-1;j>i;j--){
+    var kosong=' ',pagaratas='#',pagarbawah='#'
+    for(var j=i*2-1;j>1;j--){
       pagaratas=pagaratas+'#'
     }
-    console.log(pagaratas)
+    for(var j=i;j<num;j++){
+      kosong=kosong+' '
+    }
+    simpan.push(kosong+pagaratas)
   }
 
-  // console.log(pagarbawah)
+  for(let i=1;i<num;i++){
+    var kosong=' ',pagarbawah='#'
+    for(var j=i*2;j>0;j--){
+      if(i!=num){
+        pagarbawah=pagarbawah+'#'
+      }
+    }
+    for(var j=i+1;j<num;j++){
+      kosong=kosong+' '
+    }
+    simpan.push(kosong+pagarbawah)
+  }
+
+  return simpan
+
 }
-console.log(typeof hrglass5);  // object
-console.log(printHourglass(3));
+
+
+function printHourglass(obj){
+  for(let i=0;i<obj.length;i++){
+    console.log(obj[i])
+  }
+  return ''
+}
+let hrglass3 = generateHourglass(5);
+console.log(hrglass3);
+
+console.log(typeof hrglass3);  // object
+console.log(printHourglass(hrglass3));
 /*
 #####
  ###
