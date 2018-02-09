adcb7ab50c3def5968af41b92c00cb00479ca546
Reza Pramudhika <rezapramudhika@gmail.com>
2018-02-09 11:14:21 +0700

    commit-4

--- medium/lubang-terbesar.js
+++ medium/lubang-terbesar.js
@@ -13,25 +13,32 @@ function lubangTerbesar(arr){
     for(let i=0; i<board.length; i++){
         for(let j=0; j<board[i].length; j++){
             if(board[i][j]==0){
-                let trapHorizontal = 0;
-                for(let k=1; k<board[i].length; k++){
-                    if(board[i][k]==0){
-                        trapHorizontal += 1;
+                var trapHorizontal = 1;
+                let countPointHorizontal = 1;
+                for(let h=1; h<board[i].length-1; h++){
+                    if(board[i][h]==0){
+                        trapHorizontal += countPointHorizontal;
+                    }else if(board[i][h]==1){
+                        countPointHorizontal = 0;
                     }
                 }
                 if(trap<trapHorizontal){
                     trap = trapHorizontal;
                 }
-
-                // let trapVertical = 0;
-                // for(let k=1; k<board[i].length; k++){
-                //     if(board[j][k]==0){
-                //         trapVertical += 1;
-                //     }
-                // }
-                // if(trap<trapVertical){
-                //     trap = trapVertical;
-                // }
+                
+                var trapVertical = 1;
+                let countPointVertical = 1;
+                for(let v=1; v<board.length; v++){
+                    if(board[v][i]==0){
+                        trapVertical += countPointVertical;
+                    }else if(board[v][i]==1){
+                        countPointVertical = 0;
+                    }
+                }
+                if(trap<trapVertical){
+                    trap = trapVertical;
+                }
+                
             }
         }
     }
@@ -43,4 +50,5 @@ function lubangTerbesar(arr){
 
 console.log(lubangTerbesar(["00111", "01101", "00100", "11110"])); // 3
 console.log(lubangTerbesar(["111", "111", "111", "100"])); // 2
+console.log(lubangTerbesar(["101", "101", "101", "100"])); // 4
 console.log(lubangTerbesar(["00111", "10011", "00111", "10010","00110",'10111'])); // 6
