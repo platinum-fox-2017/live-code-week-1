18fa7c42631e7ed1c8e380ca2f4880f7d40161dc
root <irsyadpahlapi28@gmail.com>
2018-02-08 23:10:51 -0500

    upload file medium finish

--- medium/index.js
+++ medium/index.js
@@ -18,37 +18,57 @@ function lubangTerbesar(num){
   for(let i=0;i<num.length;i++){
     var simpan='',vertical=0
     for(let j=0;j<num.length;j++){
-      if(num[j][i]==='0'){
-        vertical++
+      if(num.length-1===j){
+        if(num[j][i]==='0'){
+          vertical++
+        }
+        else{
+          if(vertical>1){
+            arrvertical.push(vertical)
+            vertical=0
+          }
+        }
+      }else{
+        if(num[j][i]==='0' && num[j+1][i]==='0'){
+          vertical++
+        }
+        else{
+          if(vertical>1){
+            arrvertical.push(vertical+1)
+            vertical=0
+          }
+        }
       }
-    }
-    arrvertical.push(vertical)
-  }
 
-	for (var i = 0; i < arrhorizontal.length - 1; i++) {
+    }
+    if(vertical<num.length){
+      arrvertical.push(vertical+1)
+    }else{
+      arrvertical.push(vertical)
+    }
+  }
+	for (let i = 0; i < arrhorizontal.length - 1; i++) {
 		var min = i;
-		for (var j = i + 1; j < arrhorizontal.length; j++) {
+		for (let j = i + 1; j < arrhorizontal.length; j++) {
 			if (arrhorizontal[j] > arrhorizontal[min]) {
 				min = j;
 			}
 		}
 		if (min != i) {
-
 			var tmp = arrhorizontal[i];
 			arrhorizontal[i] = arrhorizontal[min];
 			arrhorizontal[min] = tmp;
 		}
 	}
 
-  for (var i = 0; i < arrvertical.length - 1; i++) {
+  for (let i = 0; i < arrvertical.length - 1; i++) {
 		var min = i;
-		for (var j = i + 1; j < arrvertical.length; j++) {
+		for (let j = i + 1; j < arrvertical.length; j++) {
 			if (arrvertical[j] > arrvertical[min]) {
 				min = j;
 			}
 		}
 		if (min != i) {
-
 			var tmp = arrvertical[i];
 			arrvertical[i] = arrvertical[min];
 			arrvertical[min] = tmp;
@@ -56,11 +76,13 @@ function lubangTerbesar(num){
 	}
 
   if(arrhorizontal[0]>arrvertical[0]){
-    console.log(arrhorizontal[0])
+    return arrhorizontal[0]
   }else{
-    console.log(arrvertical[0])
+    return arrvertical[0]
   }
 
 }
 
 console.log(lubangTerbesar(["00111", "01101", "00100", "11110"]))
+console.log(lubangTerbesar(["111", "111", "111", "100"])); // 2
+console.log(lubangTerbesar(["00111", "10011", "00111", "10010","00110",'10111'])); // 6
