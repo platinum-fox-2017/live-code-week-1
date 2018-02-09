623b9542cbffc44bbc634e6f112e317e3daccc59
Fachrizal Gita <Cecha@Cechas-MacBook-Pro.local>
2018-02-09 11:47:18 +0700

    waktu habis

--- challenging/paren.js
+++ challenging/paren.js
@@ -2,18 +2,23 @@ function generateParen(value){
   let arr = []
   for(let i=0; i<value; i++){
     arr.push('(')
+  }
+  for(let i=0; i<value; i++){
     arr.push(')')
   }
-  // console.log(arr.join('').split(',').join(','));
+  console.log(arr.join('').split(',').join(','));
 
-  for(let i=0; i<arr.length; i++){
+  for(let i=1; i<arr.length; i++){
     if(arr[i] === '(' && arr[i+1] === ')'){
       let temp = arr[i+1]
       arr[i+1] = arr[i]
       arr[i] = temp
       console.log(arr.join('').split(',').join(','));
     }
-    if(arr[i+1] === '(' && arr[i] === ')'){
+
+  }
+  for(let i=arr.length-1; i>0; i--){
+    if(arr[i] === '(' && arr[i+1] === ')'){
       let temp = arr[i+1]
       arr[i+1] = arr[i]
       arr[i] = temp
