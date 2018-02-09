bbd7b4a8ddcf632b2938403477700e580298a53c
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-02-09 11:47:17 +0700

    Challenging : Belum Selesai sampai akhir, baru 2 parent

--- challenging/index.js
+++ challenging/index.js
@@ -2,15 +2,41 @@
 function generateParen(num){
   var paren  = [] ;
   var parenNum = num * 2;
-  var indexDepan = num;
-  var indexBelakang = num + 1;
+  var indexDepan = num - 1 ;
+  var jumlahParen = parenNum - 1; 
+  var indexBelakang = (num - 1) + 1;
   var childParent = [];
+  var grantParent = [];
   for(var i = 0; i < parenNum; i++){
     childParent = '|';
     paren.push(childParent);
     
   }
-  console.log(paren);
+
+  for(var i = 0; i < jumlahParen; i++){
+    for(var j = 0; j < num; j++){
+      if(i === 1 && j === 0){
+        paren[indexDepan - j] = ')';
+        paren[indexBelakang + j] = '(';
+       } else {
+        if(i % 2 === 0 && i > 0 && j == num - num){
+          paren[indexBelakang + j] = ')';
+          paren[indexDepan - j] = ')';
+        } else if(i % 2 === 0 && i > 0 && j == num - 2){
+          paren[indexBelakang + j] = '(';
+          paren[indexDepan - j] = '(';
+        } else {
+          paren[indexDepan - j] = '(';
+          paren[indexBelakang + j] = ')';
+          
+        }
+      }
+    }
+    var parenJoin =paren.join('');
+    grantParent.push(parenJoin);
+    
+  }
+  console.log(grantParent);
   
 }
 
