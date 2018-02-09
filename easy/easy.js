654196bf84df47cb2939bf20a77b8f94ac9e305d
Reynaldi Ananda Pane <reynaldipane@gmail.com>
2018-02-09 11:48:53 +0700

    Add easy.js

--- easy/easy.js
+++ easy/easy.js
@@ -4,27 +4,51 @@ function generateHourglass (num) {
         counterNum = counterNum + 2;
     }
 
-    let arrHourGlass = [];
-
-    let counterArrHourGlass = 0;
-    while (counterArrHourGlass < counterNum) {
-        let hashString = '';
-        var depan       = counterArrHourGlass + 1;
-        var belakang    = counterNum - 1;
-        for(let i = 0; i < counterNum; i++) {
-            if(depan && )
-            hashString = hashString + '#';
+    var arrHourGlass = [];
+    var depan = 0;
+    var belakang = counterNum - 1;
+    for(var i = 0; i < counterNum; i++) {
+        var hash = '';
+        if(i == 0 || i == counterNum-1) {
+            for (var j = 0; j < counterNum; j++) {
+                hash = hash + '*';
+            }
+            arrHourGlass.push(hash)
+        } else if (i == depan || i == belakang) {
+            for (var j = 0; j < counterNum; j++) {
+                if(j < depan || j > belakang)
+                    hash = hash + ' ';
+                else {
+                    hash = hash + '*';
+                }
+            }
+            arrHourGlass.push(hash)
         }
-        arrHourGlass.push(hashString);
-        counterArrHourGlass = counterArrHourGlass + 1;     
+        depan = depan + 1;
+        belakang = belakang - 1;
     }
+
+    var indexTengah = (Math.floor(arrHourGlass.length / 2));
     
+    var counterTengah = indexTengah-1;
+    for (let i = indexTengah+1; i < arrHourGlass.length; i ++) {
+        arrHourGlass[i] = arrHourGlass[counterTengah];
+        counterTengah = counterTengah - 1;
+    }
+
     return arrHourGlass;
 }
 
-function printHourglass() {
 
+function printHourglass(arrHourGlass) {
+    for(let i = 0; i < arrHourGlass.length; i++) {
+        console.log(arrHourGlass[i]);
+        console.log(" ");
+        
+    }
 }
 
-let hrglass = generateHourglass(2);
+let hrglass = generateHourglass(7);
 console.log(hrglass);
+console.log(typeof hrglass);
+printHourglass(hrglass);
